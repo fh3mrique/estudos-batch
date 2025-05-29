9d4061fef4ab1cb0c475a23463bb76975c659146
Filipe Lima <fhemrique55@gmail.com>
2025-05-29 19:50:01 -0300

    primeiro commit

---
+++ src/main/java/com/estudos/helloWorldSpringBatch/HelloWorldSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.helloWorldSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/helloWorldSpringBatch/HelloWorldSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.helloWorldSpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
